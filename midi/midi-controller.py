bcfa8db1e911ff460946ce3f8337fd963390f2f7
Nick Vallejos <nvallejos1234@gmail.com>
2018-01-27 15:22:25 -0500

    Check midi folder and run midi-controller.py

---
+++ midi/midi-controller.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+
+pygame.init()
+pygame.display.set_mode((200,100))
+pygame.mixer.music.load("major-scale.mid")
+pygame.mixer.music.set_volume(1.0)
+pygame.mixer.music.play(-1)
+pygame.mixer.music.pause()
+pygame.mixer.music.stop()
