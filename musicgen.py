1b9aef88f8e74d93aebf5ac75acc5cb3b1f3a548
Nick Vallejos <nvallejos1234@gmail.com>
2018-01-27 20:02:44 -0500

    double yee

--- musicgen.py
+++ musicgen.py
@@ -4,9 +4,9 @@ from midiutil import MIDIFile
 
 def generate(filename):
     # generate
-    degrees  = generateNotes(0, 1) # This is where the actual notes are stored ... ex: [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
+    degrees  = generateNotes(0, 60, 8) # This is where the actual notes are stored ... ex: [60, 62, 64, 65, 67, 69, 71, 72]  # MIDI note number
     track    = 0	# Literally, the track at which the notes are stored
-    channel  = 0	# I don't know yet
+    channel  = 0	# 1 = mono, 2 = stereo
     time     = 0    # When the note is played (the beat at which the note is played)
     duration = 1    # 1 = quarter note, 1/2 = eigth note
     tempo    = 60   # In BPM
@@ -15,7 +15,7 @@ def generate(filename):
     MyMIDI = MIDIFile(1)  # One track, defaults to format 1 (tempo track is created
                           # automatically)
     MyMIDI.addTempo(track, time, tempo)
-
+	# Idea: every iteration of the for loop, you randomly generate a new number
     for i, pitch in enumerate(degrees):
         MyMIDI.addNote(track, channel, pitch, time + i*0.5, duration * 1/2, volume) # This is where we can configure the timing of each note
 
@@ -26,8 +26,12 @@ def generate(filename):
         MyMIDI.writeFile(output_file)
 
 # This function will generate a random sequence of notes (midi notes) and will return an array of random notes given certain constraints
-def generateNotes(lowerBound, upperBound):
-	retval = [60, 62, 64, 65, 67, 69, 71, 72]
+def generateNotes(lowerBound, upperBound, totalNotes):
+	#retval = [60, 62, 64, 65, 67, 69, 71, 72]
+	retval = [0] * totalNotes # Randomly instantiates an array of size totalNotes
+	for i in retval:
+		i = random.randint(lowerBound, upperBound)
+	
 	return retval
 
 def play(filename):
